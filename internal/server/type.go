50661bbb5d33d773636b8eafa45bad5bdb4d4126
= <=>
2023-09-03 17:49:57 +0700

    add socket hub

---
+++ internal/server/type.go
@@ -0,0 +1,18 @@
+package server
+
+import (
+	"net"
+	"sync"
+)
+
+type network = string
+type port = uint16
+
+const (
+	TCP network = "tcp"
+)
+
+type hub struct {
+	l    sync.Locker
+	list map[port]net.Listener
+}
